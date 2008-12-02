db53de1e25b0e3fe754da682d1cdbcb8ceac4ab7
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2008-12-02 13:47:42 +0100

    Generator included (code taken from jackdempsey's ArStackGenerator)

--- lib/generators/pupu.rb
+++ lib/generators/pupu.rb
@@ -1,12 +1,12 @@
 module Merb
   module Generators
-    class MoostackGenerator < AppGenerator
+    class PupuGenerator < AppGenerator
       #
       # ==== Paths
       #
 
       def self.source_root
-        File.join(super, 'application', 'moostack')
+        File.join(super, 'application', 'pupu')
       end
 
       def self.common_templates_dir
@@ -21,68 +21,46 @@ module Merb
         self.class.common_templates_dir
       end
       
-      def testing_framework
-        :rspec
-      end
-      
-      def orm
-        :datamapper
-      end
-
       #
       # ==== Generator options
       #
 
-      option :template_engine, :default => :haml,
-      :desc => 'Template engine to prefer for this application (one of: erb, haml).'
+      #option :template_engine, :default => :haml,
+      #:desc => 'Template engine to prefer for this application (one of: erb, haml).'
 
-      desc <<-DESC
-      This generates a "prepackaged" (or "opinionated") Merb application that uses ActiveRecord,
-      TestUnit, helpers, assets, mailer, caching, slices and merb-auth all out of the box.
-    DESC
+      #desc <<-DESC
+      #This generates a "prepackaged" (or "opinionated") Merb application that uses ActiveRecord,
+      #TestUnit, helpers, assets, mailer, caching, slices and merb-auth all out of the box.
+    #DESC
 
-      first_argument :name, :required => true, :desc => "Application name"
+      first_argument :name, :required => true, :desc => "Pupu name"
 
       #
       # ==== Common directories & files
       #
 
-      empty_directory :gems, 'gems'
-      file :thorfile do |file|
-        file.source      = File.join(common_templates_dir, "merb.thor")
-        file.destination = "tasks/merb.thor"
-      end
-
-      template :rakefile do |template|
-        template.source = File.join(common_templates_dir, "Rakefile")
-        template.destination = "Rakefile"
-      end
+      empty_directory :images, 'images'
+      empty_directory :javascripts, 'javascripts'
+      empty_directory :stylesheets, 'stylesheets'
 
       file :gitignore do |file|
         file.source = File.join(common_templates_dir, 'dotgitignore')
         file.destination = ".gitignore"
       end
 
-      file :htaccess do |file|
-        file.source = File.join(common_templates_dir, 'dothtaccess')
-        file.destination = 'public/.htaccess'
-      end
-      
-      file :doctask do |file|
-        file.source = File.join(common_templates_dir, 'doc.thor')
-        file.destination = 'tasks/doc.thor'
-      end
-      
-      file :mootools do |file|
-        file.source = File.join(common_templates_dir, 'mootools.js')
-        file.destination = 'public/javascripts/mootools.js'
+      template :config do |template|
+        template.source = File.join(common_templates_dir, "config.rb")
+        template.destination = "config.rb"
       end
 
-      directory :test_dir do |directory|
-        dir = testing_framework == :rspec ? "spec" : "test"
+      template :license do |template|
+        template.source = File.join(common_templates_dir, "LICENSE")
+        template.destination = "LICENSE"
+      end
 
-        directory.source      = File.join(source_root, dir)
-        directory.destination = dir
+      template :readme do |template|
+        template.source = File.join(common_templates_dir, "README.textile")
+        template.destination = "README.textile"
       end
 
       #
@@ -104,6 +82,6 @@ module Merb
       end
     end
 
-    add 'moostack',   MerbArStackGenerator
+    add 'pupu', PupuGenerator
   end
 end
---
+++ lib/generators/templates/application/common/config.rb
@@ -0,0 +1,11 @@
+# dependencies of the pupu
+#dependencies :mootools
+
+# javascripts for loading
+javascripts "<%= name %>", :in => "lib"
+
+# parameters of plugin
+# it will be blank in most cases
+#parameter :request, :optional => ["local", "ajax"] do |type|
+#  javascript "lib/autocompleter.#{type}"
+#end
