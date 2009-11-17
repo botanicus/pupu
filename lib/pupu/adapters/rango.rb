bc198779b61dbb8836944204d4b9b977949f85a2
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-11-17 18:58:49 +0000

    Updated Rango adapter

--- lib/pupu/adapters/rango.rb
+++ lib/pupu/adapters/rango.rb
@@ -2,8 +2,10 @@ require "pupu"
 require "pupu/helpers"
 
 # TODO: Mixins::Controller instead of Controller ... or better helpers
-Pupu.root = Project.root
-Pupu.media_prefix = Project.settings.media_prefix
-Pupu.media_root = Project.settings.media_root
-Rango::Controller.send(:include, Pupu::PupuHelpersMixin)
-Rango.logger.info("Pupu plugin registered")
+Rango.after_boot(:register_pupu) do
+  Pupu.root = Project.root
+  Pupu.media_prefix = Project.settings.media_prefix
+  Pupu.media_root = Project.settings.media_root
+  Rango::Controller.send(:include, Pupu::PupuHelpersMixin)
+  Rango.logger.info("Pupu plugin registered")
+end
