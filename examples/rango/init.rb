140ee9a333ecf6f4c9c86cd34cf9df51dac2aef7
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-11-28 18:02:01 +0000

    Removed some unnecessary stuff from init.rb

--- examples/rango/init.rb
+++ examples/rango/init.rb
@@ -1,32 +1,19 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
-require "rango"
-require "rango/gv"
-require "pupu/helpers"
-
 # === Usage === #
 # init.rb
-# init.rb production
-Rango.boot(environment: "development")
 
+# setup $:
 pupu_libdir = File.expand_path("../../lib")
 raise Errno::ENOENT, "#{pupu_libdir} doesn't exist" unless File.directory?(pupu_libdir)
 $:.unshift(pupu_libdir)
 
-# === Environment === #
-Pupu.root = File.dirname(__FILE__)
-Pupu.media_root = File.join(File.dirname(__FILE__), "media")
+# load and boot rango
+require "rango"
+require "pupu/adapters/rango" # this has to be loaded before rango boot
 
-Rango::Router.use(:usher)
-
-# register helpers
-Rango::GV::View.send(:include, Pupu::Helpers)
-
-Project.router = Usher::Interface.for(:rack) do
-  get("/").to(Rango::GV.static {"index"})
-  get("/examples/:template").to(Rango::GV.static { |template| "examples/#{template}" })
-end
+Rango.boot
 
 # if you will run this script with -i argument, interactive session will begin
 Rango.interactive if ARGV.delete("-i")
