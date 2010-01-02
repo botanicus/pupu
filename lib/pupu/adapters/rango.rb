c8a3b8608beb9821b6cec28457969eb8e234cf52
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2010-01-02 17:30:25 +0100

    Updated Rango adapter

--- lib/pupu/adapters/rango.rb
+++ lib/pupu/adapters/rango.rb
@@ -2,9 +2,9 @@ require "pupu"
 require "pupu/helpers"
 
 Rango.after_boot(:register_pupu) do
-  Pupu.root = Project.root
-  Pupu.media_prefix = Project.settings.media_prefix
-  Pupu.media_root = Project.settings.media_root
+  Pupu.root = Rango.root
+  #Pupu.media_prefix = Project.settings.media_prefix
+  #Pupu.media_root = Project.settings.media_root
   Rango::Helpers.send(:include, Pupu::Helpers)
   Rango.logger.info("Pupu plugin registered")
 end
