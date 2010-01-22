12dc0a75d3bdc1682d0bb21e03e6d0181e4df1f5
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2010-01-22 23:26:16 +0000

    Improved bin/pupu & updated TODO

--- tasks.rb
+++ tasks.rb
@@ -27,8 +27,6 @@ rescue LoadError
   warn "If you want to contribute to Pupu, please install code-cleaner and then run ./tasks.rb hooks:whitespace:install to get Git pre-commit hook for removing trailing whitespace."
 end
 
-require_relative "lib/pupu"
-
 # Setup encoding, so all the operations
 # with strings from another files will work
 Encoding.default_internal = "utf-8"
