71ef9f191c3fcf7c31f38f70ebc75c413684ec13
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2010-01-22 20:05:56 +0000

    Generated a new example for Ruby on Rails, the most fucking framework ever!

---
+++ examples/rails/config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_rails_session',
+  :secret      => '6bd040269fe358f0a2bc3bccd9972b9096e31a59b68902d877b3599e026fe290912d57792b67b04e6dab3eda3f67471dc03d229466495c6fc583d3fb263dcea2'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
